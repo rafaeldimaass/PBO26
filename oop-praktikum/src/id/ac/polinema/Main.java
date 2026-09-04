88012a0972c3114c913399a21f0f7fb293696ba6
rafaeldimaass <rafadimas29@gmail.com>
2026-09-04 09:45:13 +0700

    Menambahkan class student dan array of object

--- oop-praktikum/src/id/ac/polinema/Main.java
+++ oop-praktikum/src/id/ac/polinema/Main.java
@@ -2,14 +2,16 @@ package id.ac.polinema;
 
 public class Main {
     public static void main(String[] args) {
-        Rectangle original = new Rectangle(6, 4);
+        Rectangle[] shapes = new Rectangle[3];
+        shapes[0] = new  Rectangle(6, 4);
+        shapes[1] = new Rectangle(3, 3);
+        shapes[2] = new Rectangle(8, 2);
 
-        System.out.println("Area: " + original.area());
+        for (Rectangle r : shapes) {
+            System.out.println("Area: " + r.area() + ", Perimeter: " + r.perimeter());
+        }
 
-        Rectangle copy = original;
-        copy.width = 10;
-        System.out.println("Via original: " + original.area());
-        System.out.println("Via vopy: " + copy.area());
-
+        Student s = new Student("Nadia", "S001", 3.8);
+        System.out.println(s.describe());
     }
 }
