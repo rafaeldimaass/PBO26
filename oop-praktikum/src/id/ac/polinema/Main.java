7c2d1e20fd20c755bc0a4c4346da70f7fb031126
rafaeldimaass <rafadimas29@gmail.com>
2026-09-04 09:09:12 +0700

    Menambahkan konstruktor serta referensi objek, aliasing, dan uji null

--- oop-praktikum/src/id/ac/polinema/Main.java
+++ oop-praktikum/src/id/ac/polinema/Main.java
@@ -2,11 +2,16 @@ package id.ac.polinema;
 
 public class Main {
     public static void main(String[] args) {
-        Rectangle r = new Rectangle();
-        r.width = 6;
-        r.height = 4;
+        Rectangle original = new Rectangle(6, 4);
 
-        System.out.println("Area: " + r.area());
-        System.out.println("Perimeter: " + r.perimeter());
+        System.out.println("Area: " + original.area());
+
+        Rectangle copy = original;
+        copy.width = 10;
+        System.out.println("Via original: " + original.area());
+        System.out.println("Via vopy: " + copy.area());
+
+        Rectangle empty = null;
+        System.out.println(empty.area());
     }
 }
--- oop-praktikum/src/id/ac/polinema/Rectangle.java
+++ oop-praktikum/src/id/ac/polinema/Rectangle.java
@@ -3,6 +3,11 @@ package id.ac.polinema;
 public class Rectangle {
     int width, height;
 
+    Rectangle(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
     int area() {
         return width * height;
     }
