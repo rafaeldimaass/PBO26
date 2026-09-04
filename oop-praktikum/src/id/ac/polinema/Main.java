9b3e6161c84d70444f5adc4ccb3aa1989ba2083c
rafaeldimaass <rafadimas29@gmail.com>
2026-09-04 08:30:48 +0700

    Langkah 1-2 Jobsheet 2

---
+++ oop-praktikum/src/id/ac/polinema/Main.java
@@ -0,0 +1,11 @@
+package id.ac.polinema;
+
+public class Main {
+    public static void main(String[] args) {
+        Rectangle r = new Rectangle();
+        r.width = 6;
+        r.height = 4;
+
+        System.out.println("Rectangle " + r.width + "x" + r.height);
+    }
+}
---
+++ oop-praktikum/src/id/ac/polinema/Rectangle.java
@@ -0,0 +1,5 @@
+package id.ac.polinema;
+
+public class Rectangle {
+    int width, height;
+}
