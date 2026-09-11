74d364496815c8ec96a606a40c7da12c61bf3f92
rafaeldimaass <rafadimas29@gmail.com>
2026-09-11 08:46:56 +0700

    Percobaan 2 Jobsheet 3

--- MotorEncapsulation/Motor.java
+++ MotorEncapsulation/Motor.java
@@ -1,9 +1,33 @@
-
 package MotorEncapsulation;
 
 public class Motor {
-    public int kecepatan = 0;
-    public boolean kontakOn = false;
+    private int kecepatan = 0;
+    private boolean kontakOn = false;
+
+    public void nyalakanMesin() {
+        kontakOn = true;
+    }
+    
+    public void matikanMesin() {
+        kontakOn = false;
+        kecepatan = 0;
+    }
+
+    public void tambahKecepatan() {
+        if (kontakOn == true) {
+            kecepatan += 5;
+        } else {
+            System.out.println("Kecepatan tidak bisa bertambah karena mesin Off! \n");
+        }
+    }
+
+    public void kurangiKecepatan() {
+        if (kontakOn == true) {
+            kecepatan -= 5;
+        } else {
+            System.out.println("Kecepatan tidak bisa berkurang karena mesin Off! \n");
+        }
+    }
 
     public void printStatus() {
         if (kontakOn == true) {
@@ -15,3 +39,20 @@ public class Motor {
     }
     
 }
+
+// package MotorEncapsulation;
+
+// public class Motor {
+//     public int kecepatan = 0;
+//     public boolean kontakOn = false;
+
+//     public void printStatus() {
+//         if (kontakOn == true) {
+//             System.out.println("Kontak On");
+//         } else {
+//             System.out.println("Kontak Off");
+//         }
+//         System.out.println("Kecepatan " + kecepatan+"\n");
+//     }
+    
+// }
--- MotorEncapsulation/MotorDemo.java
+++ MotorEncapsulation/MotorDemo.java
@@ -4,7 +4,25 @@ public class MotorDemo {
     public static void main(String[] args) {
         Motor motor = new Motor();
         motor.printStatus();
-        motor.kecepatan = 50;
+        // motor.kecepatan = 50;
+        // motor.printStatus();
+
+        motor.tambahKecepatan();
+
+        motor.nyalakanMesin();
+        motor.printStatus();
+
+        motor.tambahKecepatan();
+        motor.printStatus();
+
+        motor.tambahKecepatan();
+        motor.printStatus();
+
+        motor.tambahKecepatan();
+        motor.printStatus();
+
+        motor.matikanMesin();
         motor.printStatus();
     }
 }
+
